f2d1c72b4ca36102cf486055ab0bfc25f8c8acd4
dasx000 <dikysaputra2025@gmail.com>
2022-12-31 14:46:18 +0700

    s

---
+++ baseikal/command/menu/menu.js
@@ -0,0 +1,33 @@
+const menu =  ` 
+° Version : 12
+° Baileys : 4.4.0
+ ▰▱▰▱▰▱▰▱▰▱▰▱▰▱
+❍ MENU_MODS ❍
+┏━━⊱
+┣❏ Tiktok
+┣❏ Belajarkita
+┣❏ Jasarun
+┣❏ Methodeband
+┣❏ Afk
+┣❏ Ping
+┣❏ Owner
+┣❏ Listwibu
+┣❏ Nowa 628xxx
+┣❏ Verif 62xxx
+┣❏ User add 62xx
+┣❏ User del 62xxx
+┣❏ Setcmd [ reply sticker ]
+┣❏ Delcmd [ reply sticker ]
+┗━━⊱
+❍ MENU_GROUP ❍
+┏━━⊱
+┣❏ Antilink on / off
+┣❏ Welcome on / off
+┣❏ Detectadmin on / off
+┣❏ Kick 628xxx
+┣❏ Add 628xxx
+┣❏ Promote 628xx
+┣❏ Demote 628xx
+┗━━⊱
+LEONBOTZ`
+exports.menu = menu
